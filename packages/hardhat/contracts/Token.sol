601c5daaf5a3b70df8f2e35053fd92194fbfb424
Claudio Hermida <claudio.alberto.hermida@protonmail.com>
2025-07-18 02:38:10 -0300

    formatted Token.sol, SimpleSwapTest.js

--- packages/hardhat/contracts/Token.sol
+++ packages/hardhat/contracts/Token.sol
@@ -2,14 +2,11 @@
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.27;
 
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Token is ERC20, Ownable {
-    constructor(string memory name, string memory symbol)
-        ERC20(name, symbol)
-        Ownable(msg.sender)
-    {}
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) Ownable(msg.sender) {}
 
     // want to mint tokens at will
     function mint(address to, uint256 amount) public onlyOwner {
